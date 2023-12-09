18796076e0cec2e19d068a4f1133355aa866cf7c
PedroJSilva2001 <pedro.jsilva322@gmail.com>
2023-12-09 01:00:47 +0000

    Add table schema filling
    Co-authored-by: Gustavo Santos <up201907397@fe.up.pt>

--- src/pt/up/fe/els2023/interpreter/symboltable/SymbolTableFiller.java
+++ src/pt/up/fe/els2023/interpreter/symboltable/SymbolTableFiller.java
@@ -1,10 +1,12 @@
 package pt.up.fe.els2023.interpreter.symboltable;
 
 import org.apache.commons.lang3.StringUtils;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
 import pt.up.fe.els2023.export.*;
 import pt.up.fe.els2023.interpreter.diagnostic.Diagnostic;
+import pt.up.fe.els2023.model.schema.*;
 import pt.up.fe.els2023.racoons.ExporterValue;
 import pt.up.fe.els2023.racoons.impl.*;
 import pt.up.fe.els2023.sources.JsonSource;
@@ -12,16 +14,16 @@ import pt.up.fe.els2023.sources.XmlSource;
 import pt.up.fe.els2023.sources.YamlSource;
 import pt.up.fe.specs.util.classmap.FunctionClassMap;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class SymbolTableFiller {
 
     private SymbolTable symbolTable;
 
+    private List<Diagnostic> warnings;
+
     private List<Diagnostic> errors;
+
 
     public SymbolTable getSymbolTable() {
         return symbolTable;
@@ -33,6 +35,7 @@ public class SymbolTableFiller {
 
     public void fill(EObject root, String racoonsConfigFilename) {
         this.symbolTable = new RacoonsSymbolTable(racoonsConfigFilename);
+        this.warnings = new ArrayList<>();
         this.errors = new ArrayList<>();
 
         fillVersion(root);
@@ -44,7 +47,6 @@ public class SymbolTableFiller {
 
         symbolTable.addVersion(version.getCode());
     }
-
 
     private void fillStatementSymbols(EObject root) {
         var statements = ((RacoonsImpl)root).getStatements();
@@ -61,7 +63,6 @@ public class SymbolTableFiller {
             map.apply(statement);
         }
     }
-
 
     private Void fillSourceDecl(SourceDeclImpl sourceDecl) {
         var name = sourceDecl.getName();
@@ -95,8 +96,192 @@ public class SymbolTableFiller {
 
     private Void fillNftDecl(NftDeclImpl nftDecl) {
         // TODO
-        System.out.println("NftDecl: " + nftDecl.getName());
+        var name = nftDecl.getName();
+        var sourceName = nftDecl.getSource().getName();
+
+        if (symbolTable.hasTableSchema(name)) {
+            errors.add(Diagnostic.error(symbolTable.getRacoonsConfigFilename(),
+                    NodeModelUtils.getNode(nftDecl).getStartLine(), -1,
+                    "Table schema with name '" + name + "' already exists"));
+            return null;
+        }
+
+        if (sourceName == null) {
+            errors.add(Diagnostic.error(symbolTable.getRacoonsConfigFilename(),
+                    NodeModelUtils.getNode(nftDecl).getStartLine(), -1,
+                    "Source specified in table schema '" + name + "' does not exist"));
+            return null;
+        }
+
+        var nftNodes = nftDecl.getNft().getNodeList();
+
+        var schema = new TableSchema(name)
+                .source(symbolTable.getSource(sourceName));
+
+        fillNftNodes(schema, nftNodes);
+
         return null;
+    }
+
+    private void fillNftNodes(TableSchema schema, EList<pt.up.fe.els2023.racoons.SchemaNode> nftNodes) {
+        var nodes = new ArrayList<SchemaNode>();
+
+        for (var nftNode : nftNodes) {
+            var schemaNode = fillSchemaNode((SchemaNodeImpl) nftNode);
+
+            if (schemaNode != null) {
+                nodes.add(schemaNode);
+            }
+        }
+        // todo diagnostics
+
+        symbolTable.addTableSchema(schema.name(), schema.nft(nodes));
+    }
+
+    private SchemaNode fillSchemaNode(SchemaNodeImpl schemaNode) {
+        FunctionClassMap<EObject, SchemaNode> map = new FunctionClassMap<>();
+
+        map.put(EachNodeImpl.class, this::fillEachNode);
+        map.put(ListNodeImpl.class, this::fillListNode);
+        map.put(PropertyNodeImpl.class, this::fillPropertyNode);
+        map.put(ResourceLocationNodeImpl.class, this::fillResourceLocationNode);
+        map.put(AllNodeImpl.class, this::fillAllNode);
+        map.put(AllValuesNodeImpl.class, this::fillAllValuesNode);
+        map.put(AllContainersNodeImpl.class, this::fillAllContainersNode);
+        map.put(ExceptNodeImpl.class, this::fillExceptNode);
+        map.put(IndexNodeImpl.class, this::fillIndexNode);
+
+        return map.apply(schemaNode);
+    }
+
+    private SchemaNode fillAllNode(AllNodeImpl allNode) {
+        var formatNode = allNode.getFormatNode();
+        return formatNode == null? new AllNode() : new AllNode(formatNode.getName());
+    }
+
+    private SchemaNode fillAllValuesNode(AllValuesNodeImpl allValuesNode) {
+        var formatNode = allValuesNode.getFormatNode();
+
+        return formatNode == null? new AllValueNode() : new AllValueNode(formatNode.getName());
+    }
+
+    private SchemaNode fillAllContainersNode(AllContainersNodeImpl allContainersNode) {
+        return new AllContainerNode();
+    }
+
+    private SchemaNode fillExceptNode(ExceptNodeImpl exceptNode) {
+        var formatNode = exceptNode.getFormatNode();
+
+        var exceptColumns = new HashSet<>(exceptNode.getElement().getNodeList());
+
+        if (formatNode == null) {
+            return new ExceptNode(exceptColumns);
+        }
+
+        if (!formatNode.getName().contains("%s")) {
+            warnings.add(Diagnostic.warning(symbolTable.getRacoonsConfigFilename(),
+                    NodeModelUtils.getNode(formatNode).getStartLine(), -1,
+                    "Format string in Except node does not contain '%s'. All columns in the Except node will be ignored"));
+        }
+
+        return new ExceptNode(exceptColumns, formatNode.getName());
+    }
+
+    private SchemaNode fillResourceLocationNode(ResourceLocationNodeImpl resourceLocationNode) {
+        var resourceNodeType = resourceLocationNode.getResourceNode();
+        var columnName = resourceLocationNode.getElement() == null? null : resourceLocationNode.getElement().getName();
+
+        return switch (resourceNodeType) {
+            case "file" -> columnName == null? new FileNode() : new FileNode(columnName);
+            case "filePath" -> columnName == null? new PathNode() : new PathNode(columnName);
+            case "directory" -> columnName == null? new DirectoryNode() : new DirectoryNode(columnName);
+            default -> throw new IllegalStateException("Unexpected value: " + resourceNodeType);
+        };
+    }
+
+    private SchemaNode fillPropertyNode(PropertyNodeImpl propertyNode) {
+        var name = propertyNode.getName();
+        var columnOrNodeElement = propertyNode.getElement();
+
+        if (columnOrNodeElement == null) {
+            return new PropertyNode(name, new NullNode());
+        }
+
+        FunctionClassMap<EObject, SchemaNode> map = new FunctionClassMap<>();
+
+        map.put(ColumnNodeImpl.class, this::fillColumnNode);
+        map.put(SchemaNodeImpl.class, this::fillSchemaNode);
+
+        var modelNode = map.apply(columnOrNodeElement.getNode());
+
+        return new PropertyNode(name, modelNode);
+    }
+
+    private SchemaNode fillColumnNode(ColumnNodeImpl columnNode) {
+        var columnName = columnNode.getName();
+
+        return new ColumnNode(columnName);
+    }
+
+    private SchemaNode fillIndexNode(IndexNodeImpl indexNode) {
+        var index = Integer.parseInt(indexNode.getIndex());
+        var columnOrNodeElement = indexNode.getElement();
+
+        if (columnOrNodeElement == null) {
+            return new IndexNode(index, new NullNode());
+        }
+
+        FunctionClassMap<EObject, SchemaNode> map = new FunctionClassMap<>();
+
+        map.put(ColumnNodeImpl.class, this::fillColumnNode);
+        map.put(SchemaNodeImpl.class, this::fillSchemaNode);
+
+        var modelNode = map.apply(columnOrNodeElement.getNode());
+
+        return new IndexNode(index, modelNode);
+    }
+
+    private SchemaNode fillListNode(ListNodeImpl listNode) {
+        FunctionClassMap<EObject, SchemaNode> map = new FunctionClassMap<>();
+
+        map.put(EachNodeImpl.class, this::fillEachNode);
+        map.put(ListNodeImpl.class, this::fillListNode);
+        map.put(PropertyNodeImpl.class, this::fillPropertyNode);
+        map.put(ResourceLocationNodeImpl.class, this::fillResourceLocationNode);
+        map.put(AllNodeImpl.class, this::fillAllNode);
+        map.put(AllValuesNodeImpl.class, this::fillAllValuesNode);
+        map.put(AllContainersNodeImpl.class, this::fillAllContainersNode);
+        map.put(ExceptNodeImpl.class, this::fillExceptNode);
+        map.put(IndexNodeImpl.class, this::fillIndexNode);
+
+        var modelNodes = new ArrayList<SchemaNode>();
+
+        for (var listElement : listNode.getNodeList()) {
+            var schemaNode = map.apply(listElement);
+
+            if (schemaNode != null) {
+                modelNodes.add(schemaNode);
+            }
+        }
+
+        return new ListNode(modelNodes);
+    }
+
+    private SchemaNode fillEachNode(EachNodeImpl eachNode) {
+        var columnOrNode = eachNode.getElement();
+
+        if (columnOrNode == null) {
+            return new EachNode(new NullNode());
+        }
+
+        FunctionClassMap<EObject, SchemaNode> map = new FunctionClassMap<>();
+
+        map.put(SchemaNodeImpl.class, this::fillSchemaNode);
+        map.put(ColumnNodeImpl.class, this::fillColumnNode);
+
+        var modelNode = map.apply(columnOrNode.getNode());
+
+        return new EachNode(modelNode);
     }
 
     private Void fillExporterDecl(ExporterDeclImpl exporterDecl) {
